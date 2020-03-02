f3c861d5e23f570d3e96e9b1611e229495d12e9f
raminsafarov <seferovramin7@gmail.com>
2020-03-02 15:51:00 +0400

    ## initial caching

--- src/main/java/notesapp/demo/Exception/ResourceNotFoundException.java
+++ src/main/java/notesapp/demo/Exception/ResourceNotFoundException.java
@@ -3,14 +3,14 @@ package notesapp.demo.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException{
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
     private String resourceName;
     private String fieldName;
     private Object fieldValue;
 
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
-        super(String.format("%s not found with %s : %s"));
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
@@ -20,23 +20,11 @@ public class ResourceNotFoundException extends RuntimeException{
         return resourceName;
     }
 
-    public void setResourceName(String resourceName) {
-        this.resourceName = resourceName;
-    }
-
     public String getFieldName() {
         return fieldName;
     }
 
-    public void setFieldName(String fieldName) {
-        this.fieldName = fieldName;
-    }
-
     public Object getFieldValue() {
         return fieldValue;
-    }
-
-    public void setFieldValue(Object fieldValue) {
-        this.fieldValue = fieldValue;
     }
 }
--- src/main/java/notesapp/demo/Service/NotesService.java
+++ src/main/java/notesapp/demo/Service/NotesService.java
@@ -4,22 +4,35 @@ import notesapp.demo.Exception.ResourceNotFoundException;
 import notesapp.demo.Model.Note;
 import notesapp.demo.Repository.NotesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@CacheConfig(cacheNames = {"notes"})
 @Service
 public class NotesService {
 
     @Autowired
     NotesRepository notesRepository;
 
+    @CachePut
     public Note saveNote(Note note){
       return  notesRepository.save(note);
     }
 
+    @Cacheable
     public Note getNote(Long noteId){
+        try {
+            // Sadece daha aydin olsun deye slowService yazdim
+            Thread.sleep(1000*5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return notesRepository.findById(noteId).orElseThrow(
                 () -> new ResourceNotFoundException("Note", "Id", noteId));
     }
@@ -30,21 +43,16 @@ public class NotesService {
 
     public Note updateNote(Note note, Long noteId){
         Note note1 = notesRepository.findById(noteId).orElseThrow( () -> new ResourceNotFoundException("Note" , "Id" , noteId));
-
         note1.setContent(note.getContent());
         note1.setTitle(note.getTitle());
-
         Note updatedNote = notesRepository.save(note1);
-
         return updatedNote;
     }
 
+    @CacheEvict(key = "#note1")
     public ResponseEntity<?> deleteNote(Long noteId){
-
         Note note1 = notesRepository.findById(noteId).orElseThrow( () -> new ResourceNotFoundException("Note" , "Id", noteId) );
-
         notesRepository.delete(note1);
-
         return ResponseEntity.ok().build();
     }
 
