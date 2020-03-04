bccdf8a183b08fa1834fc4ab8785ac3e2a9d8efc
raminsafarov <seferovramin7@gmail.com>
2020-03-04 12:26:04 +0400

    ## latest update

--- src/main/java/notesapp/demo/Controller/NotesController.java
+++ src/main/java/notesapp/demo/Controller/NotesController.java
@@ -19,33 +19,30 @@ public class NotesController {
     @Autowired
     NotesService notesService;
 
-    @CachePut(value = "notes",key="#note.id")
+    @CachePut(value="notes", key = "#result.id")
     @PostMapping("/add")
     public Note newNote(@RequestBody Note note){
         return notesService.saveNote(note);
     }
 
-    @Cacheable(value = "note", key = "#id")
+    @Cacheable(value = "notes", unless = "#result == null")
     @GetMapping("/get")
     public Note getNote(@RequestParam Long id){
        return notesService.getNote(id);
     }
 
-    @Cacheable(value = "notes", sync = true)
+    @Cacheable(value = "notes")
     @GetMapping("/all")
     public List<Note> getAllNotes(){
         return notesService.getAllNotes();
     }
 
-    @CachePut(value = "notes", key = "#noteId")
+    @CacheEvict(value = "notes", key = "#noteId")
     @PutMapping("/update/{noteId}")
     public Note updateNote(@RequestBody Note note, @PathVariable(value = "noteId") Long noteId){
         return notesService.updateNote(note, noteId);
     }
-
-    @Caching(evict = {
-            @CacheEvict(value="note", allEntries=true),
-            @CacheEvict(value="notes", allEntries=true)})
+    @CacheEvict(value="notes")
     @DeleteMapping("/delete/{noteId}")
     public ResponseEntity<?> deleteNote(@PathVariable(value = "noteId") Long id){
         return notesService.deleteNote(id);
--- src/main/java/notesapp/demo/Model/Note.java
+++ src/main/java/notesapp/demo/Model/Note.java
@@ -6,12 +6,13 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Table(name = "Notes")
 @EntityListeners(AuditingEntityListener.class)
-public class Note {
+public class Note implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
